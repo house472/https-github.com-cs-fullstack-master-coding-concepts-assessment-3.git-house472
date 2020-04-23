4c517ea667517be0f4bd4b3f45cdfaf469b2721f
house472 <morehouse472@hotmail.com>
2020-04-23 13:37:17 -0500

    banking

--- index.js.boilerplate.js
+++ index.js.boilerplate.js
@@ -1,26 +1,26 @@
-// POINT OF ENTRY
-// This file contains the basic boilerplate code to get up and running on server side and connect to MongoDB
+// // POINT OF ENTRY
+// // This file contains the basic boilerplate code to get up and running on server side and connect to MongoDB
 
-// Setup for server
-const express = require('express');
-const app = express();
+// // Setup for server
+// const express = require('express');
+// const app = express();
 
-// Setup base mount point for /api
-const api = require('./routes/api.js');
-app.use('/api', api);
+// // Setup base mount point for /api
+// const api = require('./routes/api.js');
+// app.use('/api', api);
 
-// Database stuff
-const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
-const mongoDb = 'mongodb+srv://YOURUSER:YOURPASSWORD@cluster0-mhpvz.gcp.mongodb.net/cs_database?retryWrites=true&w=majority';
-console.log(`Connecting to Mongo @ ${mongoDb}...`);
-mongoose.connect(mongoDb, {useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB crapped out'));
+// // Database stuff
+// const mongoose = require('mongoose');
+// mongoose.set('useCreateIndex', true);
+// const mongoDb = 'mongodb+srv://YOURUSER:YOURPASSWORD@cluster0-mhpvz.gcp.mongodb.net/cs_database?retryWrites=true&w=majority';
+// console.log(`Connecting to Mongo @ ${mongoDb}...`);
+// mongoose.connect(mongoDb, {useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB crapped out'));
 
-// START Listening on port
-const portNumber = 2112; // Use whatever port number you choose
-app.listen(portNumber, () => {
-    console.log(`Listening on port ${portNumber}`);
-});
+// // START Listening on port
+// const portNumber = 2112; // Use whatever port number you choose
+// app.listen(portNumber, () => {
+//     console.log(`Listening on port ${portNumber}`);
+// });
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -12,10 +12,43 @@ router.get('/test', (req, res)=>{
 });
 
 //Create account send json data
-
+// Trying to do a create a new record and getting an error. postman thinking. test case works, but not when i try to seed or create new record. 
 router.post('/', (req, res)=>{
-    res.send(`this is a create/post account`)
+    // res.send(`this is a create/post account ${req.params.account_number}`)
+// console.log('creating data')
+//     const seedData =[{
 
+//     accountNumber: 1234, 
+//     accountType: "Checking", 
+//     accountName : "Charles Gordon",
+//     accountBalance : 400,
+// },
+
+// {
+
+//     accountNumber: 5678, 
+//     accountType: "Savings", 
+//     accountName : "Charles Gordon",
+//     accountBalance : 400,
+// },
+
+// {
+
+//     accountNumber: 9012, 
+//     accountType: "Checking", 
+//     accountName : "Flash Gordon",
+//     accountBalance : 400,
+// },
+
+// {
+
+//     accountNumber: 6789, 
+//     accountType: "Savings", 
+//     accountName : "Flash Gordon",
+//     accountBalance : 400
+// }
+
+// ]
 BankAccountCollection.create(req.body, (errors, results)=>{
     errors ? res.send(errors) : res.send(results)
 })
